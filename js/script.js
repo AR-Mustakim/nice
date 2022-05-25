c5e782300ebc6e3dcc795881cbe5b69d07852300
hungryphobic <mustakim9419@gmail.com>
2022-05-25 13:58:34 +0900

    add project pages
    update the activity pages
    add project pages
    updated the other pages

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 //Get the button
 var mybutton = document.getElementById("topBtn");
+var toggleBtn = document.getElementById("navbarBtn")
+var toggleContainer = document.getElementById("niseNavbarToggler")
 
 // When the user scrolls down 200px from the top of the document,the button appear
 window.onscroll = function() {scrollFunction()};
@@ -17,3 +19,22 @@ function backToTopFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
 }
+
+function closeNavBar() {
+    toggleContainer.style.visibility = 'hidden';
+}
+
+ function openModal(clicked_id){
+  // console.log('The modal id is ---> '+clicked_id);
+  var w = window.innerWidth;
+  if(w >780){
+    var modal = document.getElementById("modal"+clicked_id);
+    modal.style.display = "block"
+  }
+ }
+
+ function closeModal(clicked_id){
+  // console.log('The modal id is ---> '+clicked_id);
+  var modal = document.getElementById(clicked_id);
+  modal.style.display = "none"
+ }
