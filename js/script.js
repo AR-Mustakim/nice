9801e3e4216454c1ba48d04b5c31d26a44fc91de
Mustakim <mustakim9419@gmail.com>
2022-05-26 10:33:56 +0900

    update the navigation bar

--- js/script.js
+++ js/script.js
@@ -2,14 +2,39 @@
 var mybutton = document.getElementById("topBtn");
 var toggleBtn = document.getElementById("navbarBtn")
 var toggleContainer = document.getElementById("niseNavbarToggler")
+var stickyNavbar = document.getElementById("navbar")
+var niseLogo = document.getElementById("navbar-nise-logo")
 
 // When the user scrolls down 200px from the top of the document,the button appear
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+  var w = window.innerWidth;
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 150) {
     mybutton.style.display = "block";
-  } else {
+    if(w>780){
+      stickyNavbar.style.height = "75px"
+      niseLogo.style.height = "70px"
+      niseLogo.style.paddingLeft = "150%"
+    }else{
+      stickyNavbar.style.height = "80px"
+      niseLogo.style.height = "60px"
+      niseLogo.style.padding = "5%"      
+    }
+  }
+  else if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
+    mybutton.style.display = "block";
+  }
+  else {
+    if(w>780){
+      stickyNavbar.style.height = "120px"
+      niseLogo.style.height = "90px"
+      niseLogo.style.paddingLeft = "100%"
+    } else{
+      stickyNavbar.style.height = "80px"
+      niseLogo.style.height = "60px"
+      niseLogo.style.padding = "5%"
+    }    
     mybutton.style.display = "none";
   }
 }
