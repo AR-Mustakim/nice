945daf3f411501a7b70bf9ca4dcaea7d94b5a008
hungryphobic <mustakim9419@gmail.com>
2022-05-27 09:17:52 +0900

    add people pages
    add peoplepages -incomplete
    - add members photo
    !! - for mobile view, there is still bug with the navigation bar

--- js/script.js
+++ js/script.js
@@ -1,41 +1,58 @@
 //Get the button
-var mybutton = document.getElementById("topBtn");
+var topButton = document.getElementById("topBtn");
 var toggleBtn = document.getElementById("navbarBtn")
 var toggleContainer = document.getElementById("niseNavbarToggler")
 var stickyNavbar = document.getElementById("navbar")
 var niseLogo = document.getElementById("navbar-nise-logo")
+var limit = Math.max(document.body.scrollHeight, document.body.offsetHeight,
+  document.documentElement.clientHeight, document.documentElement.scrollHeight,
+  document.documentElement.offsetHeight);
+var scrheight = screen.height;
+var wWidth = window.innerWidth;
+var triggerPoint = limit-scrheight*1.2;
 
-// When the user scrolls down 200px from the top of the document,the button appear
-window.onscroll = function() {scrollFunction()};
+console.log(" the  limit is ====:"+ limit);
+console.log(" the  triggerpoint  is ====:"+ triggerPoint);
+console.log(" the  document.body.scrollTop  is ====:"+ document.documentElement.scrollTop);
+
+// When the user scrolls down 300px from the top of the document,the button appear
+window.onscroll = function () { scrollFunction() };
 
 function scrollFunction() {
-  var w = window.innerWidth;
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 150) {
-    mybutton.style.display = "block";
-    if(w>780){
-      stickyNavbar.style.height = "75px"
-      niseLogo.style.height = "70px"
-      niseLogo.style.paddingLeft = "150%"
-    }else{
-      stickyNavbar.style.height = "80px"
-      niseLogo.style.height = "60px"
-      niseLogo.style.padding = "5%"      
+  
+  if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 ) {
+    topButton.style.display = "block";
+    if (wWidth > 780 &&  document.body.scrollTop < triggerPoint  &&  document.documentElement.scrollTop < triggerPoint ) {
+      stickyNavbar.style.height = "55px";
+      niseLogo.style.height = "50px";
+      niseLogo.style.paddingLeft = "150%";
+      stickyNavbar.style.opacity = "1";
+    } 
+    else if (wWidth < 781 && document.body.scrollTop < 1000) {
+      stickyNavbar.style.height = "80px";
+      niseLogo.style.height = "60px";
+      niseLogo.style.padding = "5%";
+      stickyNavbar.style.opacity = "1";
+    }
+    else{
+      stickyNavbar.style.height = "0px";
+      stickyNavbar.style.opacity = "0";
+      niseLogo.style.height = "0px";    
     }
   }
-  else if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
-    mybutton.style.display = "block";
-  }
   else {
-    if(w>780){
-      stickyNavbar.style.height = "120px"
-      niseLogo.style.height = "90px"
-      niseLogo.style.paddingLeft = "100%"
-    } else{
-      stickyNavbar.style.height = "80px"
-      niseLogo.style.height = "60px"
-      niseLogo.style.padding = "5%"
-    }    
-    mybutton.style.display = "none";
+    if (wWidth > 780) {
+      stickyNavbar.style.height = "120px";
+      niseLogo.style.height = "90px";
+      niseLogo.style.paddingLeft = "100%";
+      stickyNavbar.style.opacity = "1";
+    } else {
+      stickyNavbar.style.height = "80px";
+      niseLogo.style.height = "60px";
+      niseLogo.style.padding = "5%";
+      stickyNavbar.style.opacity = "1";
+    }
+    topButton.style.display = "none";
   }
 }
 
@@ -46,20 +63,22 @@ function backToTopFunction() {
 }
 
 function closeNavBar() {
-    toggleContainer.style.visibility = 'hidden';
+  toggleContainer.style.visibility = 'hidden';
 }
 
- function openModal(clicked_id){
+function openModal(clicked_id) {
   // console.log('The modal id is ---> '+clicked_id);
   var w = window.innerWidth;
-  if(w >780){
-    var modal = document.getElementById("modal"+clicked_id);
+  if (w > 780) {
+    var modal = document.getElementById("modal" + clicked_id);
     modal.style.display = "block"
   }
- }
+}
 
- function closeModal(clicked_id){
+function closeModal(clicked_id) {
   // console.log('The modal id is ---> '+clicked_id);
   var modal = document.getElementById(clicked_id);
   modal.style.display = "none"
- }
+}
+
+
